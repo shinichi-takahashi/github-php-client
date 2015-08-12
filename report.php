c0266be3c4f33cfcb0c81f487a09fee1b6e61dc8
shinichi-takahashi <shinichi_takahashi@lockon.co.jp>
2015-08-12 15:44:07 +0900

    report

---
+++ report.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/client/GitHubClient.php';
+
+$owner = 'EC-CUBE';
+$repo = 'ec-cube';
+
+$from = isset($_GET['from']) ? $_GET['from'] : date("Y-m-1 00:00:00");
+$to = isset($_GET['to']) ? $_GET['to'] : date("Y-m-1 00:00:00");
+
+$client = new GitHubClient();
+
+$client->setPage();
+$client->setPageSize(100);
+$data = array(
+    'state' => 'all',
+);
+$issues = $client->request("/repos/EC-CUBE/ec-cube/issues", 'GET', $data, 200, 'GitHubIssue', true);
+
+$summary = '';
+foreach ($issues as $issue) {
+    /* @var $issue GitHubIssue */
+    $summary .= "[" . $issue->getNumber() . "]: "
+        . $issue->getTitle()
+        . $issue->getUser()->getLogin()
+        . $issue->getCreatedAt()
+        . "<br />";
+}
+
+echo $summary;
